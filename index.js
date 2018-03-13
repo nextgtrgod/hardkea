238ae4693b81ff207f69176eea3429dbd7755ef4
nextgtrgod <KYakovlev7@gmail.com>
2018-03-14 02:19:09 +0300

    heroku deploy

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+
+const app = express();
+app.disable('x-powered-by');
+
+app.set('port', (process.env.PORT || 3000));
+
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+
+app.listen(app.get('port'), () => console.log(`Listening on: http://localhost:${app.get('port')}`));
