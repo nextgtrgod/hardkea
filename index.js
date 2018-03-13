fe9ae78dbd4bde8e843b9be627041fc028cf11ce
nextgtrgod <KYakovlev7@gmail.com>
2018-03-14 02:36:06 +0300

    server redirect

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 'use strict';
 
-const fs = require('fs');
-const path = require('path');
-const express = require('express');
+const fs = require('fs')
+const path = require('path')
+const express = require('express')
 
-const app = express();
-app.disable('x-powered-by');
+const app = express()
+app.disable('x-powered-by')
 
-app.set('port', (process.env.PORT || 3000));
-
-
-app.use(express.static(path.join(__dirname, 'dist')));
+app.set('port', (process.env.PORT || 3000))
 
 
-app.listen(app.get('port'), () => console.log(`Listening on: http://localhost:${app.get('port')}`));
+app.use(express.static(path.join(__dirname, 'dist')))
+
+app.get('*', (req, res) => res.sendFile(__dirname + '/dist/index.html'))
+
+
+app.listen(app.get('port'), () => console.log(`Listening on: http://localhost:${app.get('port')}`))
